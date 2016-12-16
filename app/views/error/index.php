7c6e713f39682520b2c33f813657589a48e367b1
Guiliano <test@pro.ru>
2016-12-16 22:37:46 +0400

    Добавлена вид страницы ошибки

---
+++ app/views/error/index.php
@@ -0,0 +1,12 @@
+<body>
+<div class="container text-center">
+    <div style="margin-top: 45px;" class="content-404">
+        <img src="/public/images/404/404.png" class="img-responsive" alt="" />
+        <h1><b>Запрашиваемая cтраница не найдена</b></h1>
+        <h3><b></b>Возможно, она была удалена или никогда не существовала.</h3>
+        <br><br>
+        <h3><a href="/">Вернуться на главную</a></h3>
+        <br>
+    </div>
+</div>
+</body>
