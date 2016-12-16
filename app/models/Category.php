1f4c46d21bafc9d8506eb55388cdbf1a508c155b
Guiliano <test@pro.ru>
2016-12-16 23:42:57 +0400

    Добавлена модель категорий

---
+++ app/models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\models;
+
+
+use vendor\core\base\Model;
+use vendor\core\Db;
+
+class Category extends Model
+{
+    const TABLE = 'category';
+
+    /**
+     * Получение списка категорий из таблицы БД
+     * @return array
+     */
+    public static function getListCategory()
+    {
+        $db = Db::instance();
+        $sql = 'SELECT * FROM ' . static::TABLE;
+        $result = $db->queryEach($sql, 'id');
+        return $result;
+
+
+    }
+}
