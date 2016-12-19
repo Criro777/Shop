037055b31eab68cb0ddcab66349f400583f0440b
Guiliano <test@pro.ru>
2016-12-19 18:38:22 +0400

    Добавлен метод отображения страницы профиля пользователя

---
+++ app/controllers/ProfileController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use app\models\User;
+
+class ProfileController extends AppController
+{
+    public function indexAction()
+    {
+        
+
+        if ($userId = User::isLogged()) {
+
+            $user = User::getItemById($userId);
+            
+            $this->render('profile', ['user' => $user[0]]);
+
+        } else {
+
+            header("Location: /user/login");
+        }
+    }
+
+
+}
