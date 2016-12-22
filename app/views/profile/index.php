57db784bf6ebbcb65002ed3ce16c08a718eec0c0
Guiliano <test@pro.ru>
2016-12-22 11:48:20 +0400

    Добавлен вид профиля пользователя

---
+++ app/views/profile/index.php
@@ -0,0 +1,14 @@
+<section style="margin-bottom: 18%">
+    <div class="container">
+        <div class="row">
+
+            <h1>Профиль пользователя</h1><br>
+            <h3>Привет, <i><?php echo $user->name;?>!</i></h3>
+            <ul>
+                <li><a href="/cabinet/edit">Редактировать данные</a></li>
+                <li><a href="/cabinet/history">Список покупок</a></li>
+            </ul>
+
+        </div>
+    </div>
+</section>
